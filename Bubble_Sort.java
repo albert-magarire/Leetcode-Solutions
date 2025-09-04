862345f7955fa029ced720b84976b666f00a7646
albert-magarire <betserai.magarire@lander.edu>
2025-09-04 14:26:17 -0400

    BubbleSort Optimized

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -44,4 +44,48 @@ public class Bubble_Sort {
             }
         }
     }
+
+    //different way to do it
+    // public static void bubbleSort(int[] arr) {
+    //     int n = arr.length;
+    //     for (int i = 0; i < n - 1; i++) {
+    //         for (int j = 0; j < n - i - 1; j++) {
+    //             if (arr[j] > arr[j + 1]) {
+    //                 // swap arr[j] and arr[j+1]
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j + 1];
+    //                 arr[j + 1] = temp;
+    //             }
+    //         }
+    //     }
+    //     // print the sorted array
+    //     System.out.println("\nSorted array: ");
+    //     for (int num : arr) {
+    //         System.out.print(num + " ");
+    //     }
+    // }
+
+    //most optimal way
+    // public static void bubbleSort(int[] arr) {
+    //     int n = arr.length;
+    //     boolean swapped;
+    //     for (int i = 0; i < n - 1; i++) {
+    //         swapped = false;
+    //         for (int j = 0; j < n - i - 1; j++) {
+    //             if (arr[j] > arr[j + 1]) {
+    //                 // swap arr[j] and arr[j+1]
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j + 1];
+    //                 arr[j + 1] = temp;
+    //                 swapped = true; // set swapped to true to indicate a swap occurred
+    //             }
+    //         }
+    //         if (!swapped) break; // if no two elements were swapped in the inner loop, the array is sorted
+    //     }
+    //     // print the sorted array
+    //     System.out.println("\nSorted array: ");
+    //     for (int num : arr) {
+    //         System.out.print(num + " ");
+    //     }
+    // }
 }
