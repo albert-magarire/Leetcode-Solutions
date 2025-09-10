8390c6155c858dfada1ac6d822db7553e6c36a4c
albert-magarire <betserai.magarire@lander.edu>
2025-09-10 17:11:04 -0400

    Fibonacci Recursive complete

--- Fibonacci_Recursive.java
+++ Fibonacci_Recursive.java
@@ -1,3 +1,15 @@
 public class Fibonacci_Recursive {
-    
+    public static void main(String args[]){
+        fibonacci(10);
+    }
+
+    public static int fibonacci(int n){
+        if (n == 0){
+            return 0;
+        }else if(n == 1){
+            return 1;
+        }else{
+            return fibonacci(n -1) + fibonacci(n-2);
+        }
+    }
 }
